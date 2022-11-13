8a38051bdc01dfba7e995ba4eb214640e5d23469
Dario Pešerović <d.peserovic@gmail.com>
2022-11-13 23:23:59 +0100

    set password form field to type password

--- web/src/modules/membership/forms/loginFormFields.js
+++ web/src/modules/membership/forms/loginFormFields.js
@@ -3,12 +3,14 @@ const loginFormFields = [
         name: 'username',
         label: 'Username',
         placeholder: 'Enter Username',
+        type: 'text',
         rules: 'required|string|between:5,25',
     },
     {
         name: 'password',
         label: 'Password',
         placeholder: 'Enter Password',
+        type: 'password',
         rules: 'required|string|between:5,25',
     },
 ];
--- web/src/modules/membership/forms/registerFormFields.js
+++ web/src/modules/membership/forms/registerFormFields.js
@@ -3,24 +3,28 @@ const registerFormFields = [
         name: 'username',
         label: 'Username',
         placeholder: 'Enter Username',
+        type: 'text',
         rules: 'required|string|between:5,25',
     },
     {
         name: 'email',
         label: 'Email',
         placeholder: 'Enter Email',
+        type: 'text',
         rules: 'required|email|string|between:5,25',
     },
     {
         name: 'password',
         label: 'Password',
         placeholder: 'Enter Password',
+        type: 'password',
         rules: 'required|string|between:5,25',
     },
     {
         name: 'confirmPassword',
         label: 'Confirm Password',
         placeholder: 'Confirm Password',
+        type: 'password',
         rules: 'required|string|same:password',
     }
 ];
